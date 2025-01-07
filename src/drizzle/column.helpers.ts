7633c1ed8aecec7b72427dcbb5a9802421643b59
Suryakant Sharma <suryasharma2901@gmail.com>
2025-01-07 17:38:39 +0530

    pnpm workspace monorepo introduced

--- src/drizzle/column.helpers.ts
+++
@@ -1,5 +0,0 @@
-import { timestamp } from 'drizzle-orm/pg-core';
-export const timestamps = {
-  updated_at: timestamp({ withTimezone: true }),
-  created_at: timestamp({ withTimezone: true }).defaultNow().notNull(),
-};
