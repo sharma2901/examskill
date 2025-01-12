fbbb592e6ab4bdf4eeed4ba9717aa2e6928b3d0a
Suryakant Sharma <suryasharma2901@gmail.com>
2025-01-12 18:26:06 +0530

    FE: adds tanstack query

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,27 +1,33 @@
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 
-// Import the generated route tree
-import { routeTree } from './routeTree.gen'
 
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree });
 
 // Register the router instance for type safety
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
+// Create a client
+const queryClient = new QueryClient();
+
 // Render the app
-const rootElement = document.getElementById('root')!
+const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
+  const root = ReactDOM.createRoot(rootElement);
   root.render(
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>,
-  )
+    <QueryClientProvider client={queryClient}>
+      {/*<ReactQueryDevtools />*/}
+      <StrictMode>
+        <RouterProvider router={router} />
+      </StrictMode>
+    </QueryClientProvider>
+  );
 }
--- apps/frontend/src/routes/index.tsx
+++ apps/frontend/src/routes/index.tsx
@@ -1,13 +1,29 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { useQuery } from "@tanstack/react-query";
 
 export const Route = createFileRoute('/')({
   component: Index,
 })
 
 function Index() {
+
+  const {isPending, error, data} = useQuery({
+    queryKey:['userData'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3000/users/');
+      return await response.json();
+    }
+  })
+
+  if (isPending) return 'Loading...';
+  if (error) return 'An error has occurred ' + error.message;
+
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      <div>{data[0].name}</div>
+      <div>{data[0].email}</div>
+      <div>{data[0].phone}</div>
     </div>
   )
 }
